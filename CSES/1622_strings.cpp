6cdff0a5c47d055032c212c675e049503a2be709
claude-3922 <aminabdulrehman532@gmail.com>
2026-03-15 12:00:23 +0500

    new problem

---
+++ CSES/1622_strings.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void storePermutation(const string &s, vector<string> &permutations)
+{
+    bool dupe = false;
+    for (const string &p : permutations)
+    {
+        if (p == s)
+        {
+            dupe = true;
+            break;
+        }
+    }
+
+    if (!dupe)
+    {
+        permutations.push_back(s);
+    }
+}
+
+void sortLexicographically(string &s)
+{
+    bool sorted = false;
+    while (!sorted)
+    {
+        sorted = true;
+        for (int i = 0; i < s.size() - 1; ++i)
+        {
+            if (s.at(i) > s.at(i + 1))
+            {
+                swap(s.at(i), s.at(i + 1));
+                sorted = false;
+            }
+        }
+    }
+}
+
+void getPermutations(const int &n, const string &chars, string &prefix, vector<string> &permutations)
+{
+    if (n == 1)
+    {
+        for (int i = 0; i < chars.size(); ++i)
+        {
+            string res = prefix + chars.at(i);
+            storePermutation(res, permutations);
+            // cout << prefix << chars.at(i) << "\n";
+        }
+        return;
+    }
+
+    for (int i = 0; i < chars.size(); ++i)
+    {
+        string tPrefix = prefix;
+        tPrefix += chars.at(i);
+
+        string tChars = chars;
+        tChars.erase(tChars.begin() + i);
+
+        getPermutations(n - 1, tChars, tPrefix, permutations);
+    }
+}
+
+int main()
+{
+    string s = "aabac", prefix;
+    vector<string> permutations;
+
+    sortLexicographically(s);
+    getPermutations(s.size(), s, prefix, permutations);
+
+    cout << permutations.size() << "\n";
+    for (const string &p : permutations)
+    {
+        cout << p << "\n";
+    }
+    return 0;
+}
